0ef33a6ae6288ece97b745556fadc3c34c5bc5db
Oikake <nguyenlyphat301202@gmail.com>
2023-10-25 23:29:59 +0700

    add home screen to app

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import HomeScreen from "./screens/home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />  
+          </Stack.Navigator>  
+        </NavigationContainer>
     );
 }
---
+++ screens/home.js
@@ -0,0 +1,16 @@
+/**
+    * HomeScreen: list of services firebase we use
+*/
+
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function HomeScreen() {
+  return (
+    <View>
+        <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
